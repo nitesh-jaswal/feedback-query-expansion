ca27030c0c5a04d48309d13fbcd12af6ae840347
nitesh-jaswal <nitesh1511@gmail.com>
2019-11-06 01:58:04 -0500

    -Initial Commit #2

---
+++ src/rocchioFeedback.java
@@ -0,0 +1,9 @@
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.benchmark.quality.trec.TrecTopicsReader;
+
+public class rocchioFeedback {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
